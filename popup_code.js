3c7b8c68baf2901aa2e8a2c74d9fe8fd586513da
clfisher <colin@fisher.io>
2023-09-08 22:41:43 -0600

    Fix silly naming mishap
    oops lol

--- popup_code.js
+++ popup_code.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    var javascript_test_button = document.getElementById("grid-container")
-    javascript_test_button.addEventListener("click", function (e) {
+    var grid_container = document.getElementById("grid-container")
+    grid_container.addEventListener("click", function (e) {
         var target = e.target
         var row = target.attributes['data-row'].value
         var column = target.attributes['data-col'].value
